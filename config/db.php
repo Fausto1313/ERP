10d8736f80e0a2e8a52448238f277fcaf0fd5679
faem1 <faem1@192.168.1.74>
2020-05-27 14:51:44 -0500

    Conexion Mysql

--- config/db.php
+++ config/db.php
@@ -1,12 +1,11 @@
 <?php
 
 return [
-  'class' => 'yii\db\Connection',
-   'dsn' => new \yii\db\mssql\PDO('sqlsrv:server=DESKTOP-128IPTR:1433\ERP;database=ERP,"username", "password"'),
-  'username' => 'sa',
-  'password' => '20763093d',
-  'charset' => 'utf8',
-  'emulatePrepare'=>'false',
-    
+     'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost:3306;dbname=ERP',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+  
 ];
 
