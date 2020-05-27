dd830db18d735a3382dcbf9a1d2c1814ce4bb2fc
faem1 <faem1@192.168.1.74>
2020-05-27 10:47:52 -0500

    Conexion Sql Server

--- config/db.php
+++ config/db.php
@@ -1,36 +1,12 @@
 <?php
 
 return [
-  /* 'class' => 'yii\db\Connection',
-   'dsn' => new \yii\db\mssql\PDO('sqlsrv:server=DESKTOP-128IPTR\ERP;database=ERP,"username", "password"'),
+  'class' => 'yii\db\Connection',
+   'dsn' => new \yii\db\mssql\PDO('sqlsrv:server=DESKTOP-128IPTR:1433\ERP;database=ERP,"username", "password"'),
   'username' => 'sa',
   'password' => '20763093d',
   'charset' => 'utf8',
   'emulatePrepare'=>'false',
-    */
-       'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost:3306;dbname=ERP',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-/*   'yii/db'=>array(
-
-	'class'=>'CDbConnection',
-
-	'connectionString'=>'sqlsrv:host=DESKTOP-128IPTR\ERP;port=1433;dbname=ERP',
-
-	'username'=>'sa',
-
-	'password'=>'20763093d',
-
-	'charset' => 'utf8',
-
-'emulatePrepare'=>'false'),
-*/    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-
     
 ];
 
