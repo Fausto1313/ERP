0a02e35c19f3e0ba3406971c8dd49a7c18c0fe87
Jliberatomex <61291518+Jliberatomex@users.noreply.github.com>
2020-06-07 03:07:50 -0500

    Cambios al  Main en la vista
    Cambios al  Main en la vista principal

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -2,7 +2,6 @@
 
 /* @var $this \yii\web\View */
 /* @var $content string */
-//se aplico cambios al main
 
 use app\widgets\Alert;
 use yii\helpers\Html;
@@ -10,12 +9,13 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
- use kartik\sidenav\SideNav;
- use kartik\icons\Icon;
+use kartik\sidenav\SideNav;
+use kartik\icons\Icon;
 
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
+
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
@@ -26,22 +26,169 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
+<br></br>
+
+ <header id="header"><!--header-->
+        <div class="header_top"><!--header_top-->
+            <!--<div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="contactinfo">
+                            <ul class="nav nav-pills">
+                                <li><a href="#"><i class="fa fa-company"></i> Reintegración en Servicios de Cómputo S.A. de C.V.</a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>-->
+        </div><!--/header_top-->
+        
+        <div class="header-middle"><!--header-middle-->
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="logo pull-left">
+                            <a href="index"><img src="images/home/RISC.png" alt="Reintegración" width="100" height="100"/></a>                            
+                        </div>
+                       
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="shop-menu pull-right">
+                            <ul class="nav navbar-nav">
+                                <li><a href="/index"><i class="fa fa-home"></i> Inicio</a></li>
+                                <li><a href="#"><i class="fa fa-star"></i> Servicios</a></li>
+                                <li><a href="#"><i class="fa fa-shopping-cart"></i> WIMO</a></li>
+                                <li><a href="#"><i class="fa fa-user"></i> Contáctenos</a></li>
+                                <li><a href="#"><i class="fa fa-window-restore"></i> Presentaciones</a></li>
+                                <li><a href="/login"><i class="fa fa-lock"></i> Login</a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--/header-middle-->
+
+
+
+
 <body>
-<?php $this->beginBody() ?>
+
+ <section id="slider"><!--slider-->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div id="slider-carousel" class="carousel slide" data-ride="carousel">
+                        <br>
+                        <ol class="carousel-indicators">
+                            <li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#slider-carousel" data-slide-to="1"></li>
+                            <li data-target="#slider-carousel" data-slide-to="2"></li>
+                        </ol>
+                        
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <div class="col-sm-6">
+                                    <h1>RISC</h1>
+                                    <h2>Más de 10 Años deCalidad en el Servicio nos respaldan </h2>
+                                    
+                                    <button type="button" class="btn btn-default get">Contactenos</button>
+                                </div>
+                                <div class="col-sm-6">
+                                    <img src="images/home/img113.jpg" class="girl img-responsive" alt="" />
+                                   
+                                </div>
+                            </div>
+
+
+                                                      
+                            <div class="item">
+                                <div class="col-sm-6">
+                                    <h1>RISC</h1>
+                                    <h2>Servicios</h2>
+                                   
+                                    <button type="button" class="btn btn-default get">Conoce más</button>
+                                </div>
+                                <div class="col-sm-6">
+                                    <img src="images/home/soporte-tecnico.jpg" class="girl img-responsive" alt="" />
+                                    
+                                </div>
+                            </div>
+
+                            <div class="item">        
+                                <div class="col-sm-6">
+                                    <h1>RISC</h1>
+                                    <h2>Lansa </h2>
+                                    
+                                    <button type="button" class="btn btn-default get">Conoce más</button>
+                                </div>
+                               <br>
+                               <br>
+                               <br>
+                                <div class="col-sm-6">
+                                    <img src="images/home/574.png" class="girl img-responsive" alt="" />
+                                   
+                                </div>                         
+                            </div>
+
+                            <div class="item">
+                                <div class="col-sm-6">
+                                    <h1>RISC</h1>
+                                    <h2>Clientes</h2>
+                                    
+                                    <button type="button" class="btn btn-default get">Contactanos</button>
+                                </div>
+                                <div class="col-sm-6">
+                                    <img src="images/home/600.jpg" class="girl img-responsive" alt="" />
+                                    
+                                </div>
+                            </div>
+
+                             <div class="item">        
+                                <div class="col-sm-6">
+                                    <h1>RISC</h1>
+                                    <h2></h2>
+                                    
+                                    <button type="button" class="btn btn-default get">Contactanos</button>
+                                </div>
+                                <div class="col-sm-6">
+                                    <img src="images/home/576.png" class="girl img-responsive" alt="" />
+                                   
+                                </div>                         
+                            </div>
+                            
+                        </div>
+                        
+                        <a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+                            <i class="fa fa-angle-left"></i>
+                        </a>
+                        <a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section><!--/slider-->
+
+
+<!--<?php $this->beginBody() ?>
 
 <div class="wrap">
     
     <?php
-    NavBar::begin([
+   NavBar::begin([
         //'brandLabel' => Yii::$app->name,
-        'brandLabel' => Html::img('./web/assets/img/RISC.png'),
+        'brandLabel' => Html::img('./web/images/home/RISC.png'),
         'brandLabel' => 'ERP RISC',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    /*En este apartado sedaran de alta las vistas, no es necesario agregar el.php*/
 
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
@@ -64,13 +211,18 @@ AppAsset::register($this);
         ],
     ]);
     NavBar::end();
-    ?>
+    ?>-->
+
+
+
+
     <div class="container">
     <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
         <div class="col-xs-5 col-sm-4 col-lg-3" >
+        
         <?php 
              echo SideNav::widget([      
 
@@ -93,48 +245,37 @@ AppAsset::register($this);
                         'icon' => 'user',
                         'url' => 'http://localhost:8080/index.php?r=crm-lead%2Findex',
                         'items' => [
-                            ['label' => 'Clientes', 'icon'=>'user', 'url'=>'#'],
-                            ['label' => 'Oportunidades', 'icon'=>'user', 'url'=>'#'],
-                            ['label' => 'Presupuesto', 'icon'=>'user', 'url'=>'#'],
-                            ['label' => 'Equipo de Ventas', 'icon'=>'user', 'url'=>'#'],
+                            ['label' => 'Clientes', 'icon'=>'user', 'url'=>'http://localhost:8080/index.php?r=res-partner%2Findex'],
+                            ['label' => 'Oportunidades', 'icon'=>'user', 'url'=>'http://localhost:8080/index.php?r=crm-lead%2Findex'],
+                            ['label' => 'Presupuesto', 'icon'=>'user', 'url'=>'http://localhost:8080/index.php?r=sale-order%2Findex'],
+                            ['label' => 'Equipo de Ventas', 'icon'=>'user', 'url'=>'http://localhost:8080/index.php?r=crm-team%2Findex'],
                         ]
                         
                     ],
                     [
-                        'label' => 'Contactos',
+                        'label' => 'Empleados',
                         'icon' => 'book',
-                        'url' => 'http://localhost:8080/index.php?r=res-partner%2Findex',
+                        'url' => 'http://localhost:8080/index.php?r=res-employed%2Findex',
                     ],
+
                     [
                         'label' => 'Ventas',
                         'icon' => 'briefcase',
-                        'url' => 'http://localhost:8080/index.php?r=crm-team%2Findex',
+                        'url' => 'http://localhost:8080/index.php?r=product-product%2Findex',
+                        'items' => [
+                            ['label' => 'Productos', 'icon'=>'product', 'url'=>'http://localhost:8080/index.php?r=product-product%2Findex'],
+                            ['label' => 'Categoria de  Productos', 'icon'=>'product', 'url'=>'http://localhost:8080/index.php?r=product-category%2Findex'],
+                        ]
+                        
                     ],
-                    [
-                        'label' => 'Presupuesto',
-                        'icon' => 'stats',
-                        'url' =>'http://localhost:8080/index.php?r=sale-order%2Findex',
-                    ],
-                    [
-                        'label' => 'Empleados',
-                        'icon' => 'user',
-                        'url' =>'http://localhost:8080/index.php?r=res-employed%2Findex',
-                    ],
+               
+                
                     [
                         'label' => 'Usuarios',
                         'icon' => 'user',
                         'url' =>'http://localhost:8080/index.php?r=res-users%2Findex',
                     ],
-                    [
-                        'label' => 'Productos',
-                        'icon' => 'briefcase',
-                        'url' =>'http://localhost:8080/index.php?r=product-product%2Findex',
-                    ],
-                    [
-                        'label' => 'Categoria de Productos',
-                        'icon' => 'stats',
-                        'url' =>'http://localhost:8080/index.php?r=product-category%2Findex',
-                    ],
+                    
                     [
                         'label' => 'Compañias',
                         'icon' => 'briefcase',
@@ -161,7 +302,56 @@ AppAsset::register($this);
      </div> <!-- cierra container -->   
     
 </div>
+<br>
 
+ <footer id="footer"><!--Footer-->
+     
+        
+        <div class="footer-widget">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="single-widget">
+                            <h2>NUESTROS PRODUCTOS Y SERVICIOS</h2>
+                            <ul class="nav nav-pills nav-stacked">
+                                <li><a href="#">Inicio</a></li>
+                                <li><a href="#">Soporte de chat en vivo</a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="single-widget">
+                            <h2>CONTACTESE CON NOSOTROS</h2>
+                            <ul class="nav nav-pills nav-stacked">
+                                <li><a href="#">Contáctenos</a></li>
+                                <li><a href="#">Lista de Correo</a></li>
+                                <li><a href="#">Empleos</a></li>
+                                <li><a href="#">Presentaciones</a></li>
+                                <li><a href="http://helpdesk.addar.mx/contacto_form/">Contacto de Soporte</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="single-widget">
+                            <h2>REINTEGRACIÓN EN SERVICIOS DE CÓMPUTO, S.A. DE C.V.</h2>
+                             <p>Somos un equipo de profesionales, nuestra meta es ayudar a mejorar los procesos, a través de las integraciones que ofrecemos, con productos disruptivos.
+                Construimos grandes productos para solucionar y optimizar, problemas de negocio, en la pequeña y mediana empresa. </p>
+
+                <p>Aviso de Privacidad</p>
+                            
+                        </div>
+                    </div>
+                   
+                    
+                    
+                </div>
+            </div>
+        </div>
+        
+        
+        
+    </footer><!--/Footer-->
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; Reintegración en Servicios de Cómputo S.A. de C.V. <?= date('Y') ?></p>
